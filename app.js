476832a89a0299d2c41e182d57889fde6d235b53
Laryssa Vazquez Santos <66438713+laryssavazquez@users.noreply.github.com>
2022-10-26 20:05:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const botaoPonto = document.getElementById("botaoPonto")
+
+botaoPonto.addEventListener("click", () => {
+    const nome = document.getElementById("nome").value
+    const matricula = document.getElementById("matricula").value
+
+    console.log(nome, matricula)
+    
+}
+)
